b5fb729463fc4bfe78291c5303394a8e69156fcc
TienPhuc02 <dtp2352002@gmail.com>
2023-10-26 14:23:16 +0700

    onboarding page

--- app/onboarding/page2/index.tsx
+++ app/onboarding/page2/index.tsx
@@ -1,38 +1,30 @@
-import { router } from "expo-router";
 import React from "react";
 import { Image, Text, View } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
 
-const OnBoarding = () => {
-  const handleClickButtonNext = () => {
-    router.replace("/onboarding/page2");
-  };
+const OnBoardingPage2 = () => {
   return (
     <View className="bg-black h-full">
       <Image
         className="mb-[40px]"
-        source={require("../../assets/images/Illustartion.png")}
+        source={require("../../../assets/images/Illustration2.png")}
       />
       <View className="max-w-[200px] max-h-[50px] mx-auto">
         <Text className="text-white font-bold text-[20px] text-center mb-[20px]">
-          Find your Comfort Food here
+          Food Ninja Is Where Your Comfort Food Lives
         </Text>
       </View>
       <View>
         <Text className="text-white text-center max-w-[240px] mx-auto font-thin mb-[40px]">
-          Here You Can find a chef or dish for every taste and color.Enjoy!
+          Enjoy a fast and smooth food delivery at your doorstep
         </Text>
       </View>
-      <TouchableOpacity
-        onPress={handleClickButtonNext}
-        className="max-w-[160px] max-h-[60px] mx-auto"
-      >
+      <View className="max-w-[160px] max-h-[60px] mx-auto">
         <Text className="text-white bg-[#28cb7d] py-[18px] px-[60px] rounded-[15px]">
           Next
         </Text>
-      </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
-export default OnBoarding;
+export default OnBoardingPage2;
