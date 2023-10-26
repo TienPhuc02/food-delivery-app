20b908eb06a99caf9a2da480138740f36bba408b
TienPhuc02 <dtp2352002@gmail.com>
2023-10-26 09:43:41 +0700

    set up tailwindcss

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,9 @@ export default function Page() {
   return (
     <View style={styles.container}>
       <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
+        <Text className={"text-red-300 text-[64px] font-bold "}>
+          Hello World
+        </Text>
         <Text style={styles.subtitle}>This is the first page of your app.</Text>
       </View>
     </View>
@@ -23,10 +25,10 @@ const styles = StyleSheet.create({
     maxWidth: 960,
     marginHorizontal: "auto",
   },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
+  // title: {
+  //   fontSize: 64,
+  //   fontWeight: "bold",
+  // },
   subtitle: {
     fontSize: 36,
     color: "#38434D",
